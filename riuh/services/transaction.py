f4c7ba1c01170a7f2b38938334f7ca0176b7154d
Mateus Santos <a.mateussm1.5@gmail.com>
2023-06-06 17:36:10 -0300

    Create Transaction Service

---
+++ riuh/services/transaction.py
@@ -0,0 +1,110 @@
+from typing import List
+
+from models.transaction import Transaction
+from models.db import db
+
+from flask_smorest import abort
+from sqlalchemy.exc import (
+    IntegrityError,
+    SQLAlchemyError,
+)
+
+class TransactionService:
+    """Service for Transaction."""
+    def __init__(self):
+        self.transaction = Transaction()
+
+
+    def get_all(self):
+        """Get all transactions."""
+        return self.transaction.query.all()
+
+
+    def get_by_id(
+            self,
+            id: int,
+    ) -> Transaction:
+        """Get an transaction by ID."""
+        return self.transaction.query.get_or_404(id)
+
+
+    def get_all_by_client_id(
+            self,
+            client_id: int,
+    ) -> List[Transaction]:
+        """Get all transactions by client_id."""
+        return self.transaction.query.filter_by(client_id=client_id).all()
+
+
+    def get_all_by_employee_id(
+            self,
+            employee_id: int,
+    ) -> List[Transaction]:
+        """Get all transactions by employee_id."""
+        return self.transaction.query.filter_by(employee_id=employee_id).all()
+
+
+    def get_all_by_type(
+            self,
+            _type: str,
+    ) -> List[Transaction]:
+        """Get all transactions by type."""
+        return self.transaction.query.filter_by(type=_type).all()
+
+
+    def create(
+            self,
+            client_id: int, employee_id: int,
+            amount: float, _type: str
+    ) -> Transaction:
+        """Create a new transaction."""
+        self.transaction.client_id = client_id
+        self.transaction.employee_id = employee_id
+        self.transaction.amount = amount
+        self.transaction.type = _type
+
+        try:
+            db.session.add(self.transaction)
+            db.session.commit()
+            return self.transaction
+        except IntegrityError:
+            abort(400, message='Transaction already exists.')
+        except SQLAlchemyError as exception:
+            abort(400, message=str(exception))
+
+
+    def update(
+            self,
+            id: int,
+            client_id: int, employee_id: int,
+            amount: float, _type: str
+    ) -> Transaction:
+        """Update an transaction."""
+        self.transaction = self.get_by_id(id)
+        self.transaction.client_id = client_id
+        self.transaction.employee_id = employee_id
+        self.transaction.amount = amount
+        self.transaction.type = _type
+
+        try:
+            db.session.commit()
+            return self.transaction
+        except IntegrityError:
+            abort(400, message='Transaction already exists.')
+        except SQLAlchemyError as exception:
+            abort(400, message=str(exception))
+
+
+    def delete(
+            self,
+            id: int,
+    ) -> Transaction:
+        """Delete an transaction."""
+        self.transaction = self.get_by_id(id)
+
+        try:
+            db.session.delete(self.transaction)
+            db.session.commit()
+            return self.transaction
+        except SQLAlchemyError as exception:
+            abort(400, message=str(exception))
