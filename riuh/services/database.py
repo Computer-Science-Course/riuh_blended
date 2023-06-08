fb227b84c910a685d731bb69abb8b0672ffd025c
Mateus Santos <a.mateussm1.5@gmail.com>
2023-06-07 22:48:59 -0300

    Fill transactions data

--- riuh/services/database.py
+++ riuh/services/database.py
@@ -1,11 +1,18 @@
+"""Database service module."""
+from random import (
+    randint,
+    choice,
+)
+
 from services import (
     ClientService,
     EmployeeService,
-    WalletService,    
+    TransactionService,
+    WalletService,
 )
 
 def populate_clients():
-    """Populate the employees table with some default data."""
+    """Populate the clients table with some default data."""
 
     clients: list = [
         {
@@ -78,32 +85,53 @@ def populate_clients():
             new_client = service.create(**client)
             wallet_service: WalletService = WalletService()
             wallet_service.create(client_id=new_client.id)
-     
+
 
 def populate_employees():
     """Populate the employees table with some default data."""
 
     employees: list = [
         {
-            'name': 'Admin',
-            'document': '00000000000',
-            'username': 'admin',
-            'password': 'admin',
+            'name': 'Mateus Santos',
+            'document': '123456789',
+            'username': 'matcraft',
+            'password': '1234',
             'active': True
         },
         {
-            'name': 'Employee',
-            'document': '11111111111',
-            'username': 'employee',
-            'password': 'employee',
+            'name': 'Joana Malheiros',
+            'document': '987654321',
+            'username': 'joanas',
+            'password': '1234',
             'active': True
         },
         {
-            'name': 'Bartolomeu',
-            'document': '11111111111',
-            'username': 'bartolomeu',
-            'password': 'bartolomeu',
+            'name': 'Rafaela Almeida',
+            'document': '456789012',
+            'username': 'rafaela',
+            'password': '1234',
+            'active': False
+        },
+        {
+            'name': 'Ricardo Santos',
+            'document': '901234567',
+            'username': 'ricardos',
+            'password': '1234',
             'active': True
+        },
+        {
+            'name': 'Fernanda Oliveira',
+            'document': '345678901',
+            'username': 'fernandao',
+            'password': '1234',
+            'active': True
+        },
+        {
+            'name': 'Gustavo Pereira',
+            'document': '678901234',
+            'username': 'gustavop',
+            'password': '1234',
+            'active': False
         },
     ]
 
@@ -114,7 +142,35 @@ def populate_employees():
         except Exception:
             service.create(**employee)
 
+
+def populate_transactions():
+    """Populate the transactions table with some default data."""
+
+    transactions: list = [
+        {
+            'client_id': randint(1, 10),
+            'employee_id': randint(2, 6),
+            'amount': randint(1, 100),
+            '_type': choice(['CREDIT', 'WITHDRAWAL', 'BUY']),
+        }
+        for _ in range(20)
+    ]
+
+    for transaction in transactions:
+        try:
+            client_service: ClientService = ClientService()
+            employee_service: EmployeeService = EmployeeService()
+
+            client_service.get_by_id(transaction.get('client_id'))
+            employee_service.get_by_id(transaction.get('employee_id'))
+
+            service: TransactionService = TransactionService()
+            service.create(**transaction)
+        except Exception:
+            print('Error creating transaction.')
+
 def populate_database():
     """Populate the database with some default data."""
     populate_employees()
     populate_clients()
+    populate_transactions()
