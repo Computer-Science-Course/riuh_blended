77a90f6f557c844dd1aa12f786bfe447cadf12fc
Mateus Santos <a.mateussm1.5@gmail.com>
2023-12-04 16:22:40 -0300

    Create Footer component

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,12 @@
+const containerStyles = 'flex justify-center bg-black-700 text-white-800 p-3 rounded-lg items-center w-full h-max'
+
+const Button = (): JSX.Element => {
+
+  return (
+    <footer className={containerStyles}>
+      Made with ❤️ by Mateus Santos
+    </footer>
+  )
+}
+
+export default Button;
