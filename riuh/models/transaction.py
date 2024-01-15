2bfada69c9f9f1c4c00f4bc2bc9ee05595d83337
Mateus Santos <a.mateussm1.5@gmail.com>
2024-01-15 08:58:27 -0300

    Fix datetime of a transaction

--- riuh/models/transaction.py
+++ riuh/models/transaction.py
@@ -7,6 +7,6 @@ class Transaction(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     client_id = db.Column(db.Integer, db.ForeignKey('clients.id'))
     employee_id = db.Column(db.Integer, db.ForeignKey('employees.id'))
-    datetime = db.Column(db.DateTime, nullable=False, default=datetime.now())
+    datetime = db.Column(db.DateTime, nullable=False, default=datetime.now)
     amount = db.Column(db.Float, nullable=False)
     transaction_type = db.Column(db.String(10), nullable=False)
