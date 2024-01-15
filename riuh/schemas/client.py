0e29993a7e2c20606c67ba0037ab94c6579f45ed
Mateus Santos <a.mateussm1.5@gmail.com>
2024-01-15 08:58:51 -0300

    Add course on Client schemas

--- riuh/schemas/client.py
+++ riuh/schemas/client.py
@@ -4,12 +4,14 @@ class ViewClientSchema(Schema):
     id = fields.Int(dump_only=True)
     name = fields.Str(dump_only=True)
     registration = fields.Str(dump_only=True)
+    course = fields.Str(dump_only=True)
     active = fields.Bool(dump_only=True)
 
 
 class CreateClientSchema(Schema):
     name = fields.Str(required=True)
     registration = fields.Str(required=True)
+    course = fields.Str(required=True)
     username = fields.Str(required=True)
     active = fields.Bool(required=False)
 
