1f312484c6ca95523ccebf2b7c528443b5ab98ab
Mateus Santos <a.mateussm1.5@gmail.com>
2024-05-31 17:25:02 -0300

    Use docker for development

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -5,4 +5,11 @@ import tailwindcss from 'tailwindcss';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    watch: {
+      usePolling: true,
+    },
+    host: true, // Here
+    strictPort: true,
+  }
 })
